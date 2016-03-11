f3e7a595610d5c5b231b2dca56245f3c87769396
Yilen <yilen.pan@gmail.com>
2016-03-10 18:12:27 -0800

    added sudoku solver problem

---
+++ sudoku/getBoard.js
@@ -0,0 +1,32 @@
+var http = require('http');
+
+// converts rawText Board to a 2d Array
+var rawToJSON = function (rawBoard) {
+  return rawBoard.split('\n').map(function (row) {
+    return row.split('|').slice(1).slice(0, -1).reduce(function (row, col) {
+      var a = col[1] === ' ' ? ' ' : parseInt(col[1]);
+      var b = col[5] === ' ' ? ' ' : parseInt(col[5]);
+      var c = col[9] === ' ' ? ' ' : parseInt(col[9]);
+      return row.concat([a, b, c]);
+    }, []);
+  }).filter(function (n, i) {
+    return i % 2 !== 0 && n.length;
+  });
+};
+
+module.exports = function (solver) {
+  // I'm lazy. Here are premade sudoku boards that will pass.
+  http.get({
+    hostname: "davidbau.com",
+    port: 80,
+    path: '/generated/sudoku.txt'
+  }, function (response) {
+    var body = '';
+    response.on('data', function (chunk) {
+      body += chunk;
+    });
+    response.on('end', function () {
+      solver(rawToJSON(body));
+    });
+  });
+};
