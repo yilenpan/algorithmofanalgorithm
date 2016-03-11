b7a9035a900e8a3c2ee11cdba7169441b9818c5a
Yilen <yilen.pan@gmail.com>
2016-03-10 18:26:59 -0800

    houseRobbers problem

---
+++ houseRobbers.js
@@ -0,0 +1,22 @@
+/////////////////////////////////////////////////////////////
+// House Robbers
+//
+// You are given a list of homes with dollar amounts associated with them
+// If you rob one house, you can't rob the houses next to them.
+//
+// Ex. [a, b, c]
+// if I rob house 'a', I can't rob house b
+// if I rob house 'b', I can't rob a or c
+// if I rob house 'c', I can't rob house b
+//
+// Your goal is to tell me the max dollar amount that I can make
+//
+/////////////////////////////////////////////////////////////
+
+var testCase = [123, 12345, 1, 2, 543, 41];
+
+var houseRobbers = function (houses) {
+  // TODO: Implement me!
+};
+
+console.log(houseRobbers(testCase) === 12888);
